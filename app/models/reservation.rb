f2b64a3eef60c39860a26e1dc1eae8e9e906e827
Marcin Zadka <gamboox@gmail.com>
2019-12-23 03:31:48 +0100

    Add status methods in Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,17 @@
 class Reservation < ApplicationRecord
   has_many :ticket_reservations
+  STATUSES = {
+    new: 0,
+    paid: 1
+  }.freeze
+
   has_many :ticket_types, through: :ticket_reservations
 
   validates :status, presence: true
+
+  STATUSES.keys.each do |status|
+    define_method "#{status}?" do
+      status == STATUSES[status]
+    end
+  end
 end
