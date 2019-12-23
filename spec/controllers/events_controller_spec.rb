92f9e84186719c924fdc0af85f516ab4d5d37fe0
Marcin Zadka <gamboox@gmail.com>
2019-12-23 13:58:15 +0100

    Test events_controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -24,27 +24,11 @@ require 'rails_helper'
 # `rails-controller-testing` gem.
 
 RSpec.describe EventsController, type: :controller do
-
-  # This should return the minimal set of attributes required to create a valid
-  # Event. As you add validations to Event, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    { name: 'Event one', date: DateTime.now + 1.year }
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # EventsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:event) { create(:event) }
 
   describe "GET #show" do
     it "returns a success response" do
-      event = Event.create! valid_attributes
-      get :show, params: {id: event.to_param}, session: valid_session
+      get :show, params: {id: event.to_param}
       expect(response).to be_successful
     end
   end
