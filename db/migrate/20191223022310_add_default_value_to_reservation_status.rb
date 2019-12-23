392c17c06661f27d64ee2fcb6c3161079750aee9
Marcin Zadka <gamboox@gmail.com>
2019-12-23 03:32:11 +0100

    Set default Reservation status

---
+++ db/migrate/20191223022310_add_default_value_to_reservation_status.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToReservationStatus < ActiveRecord::Migration[6.0]
+  def change
+    change_column :reservations, :status, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_004833) do
+ActiveRecord::Schema.define(version: 2019_12_23_022310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_004833) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.integer "status", null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
