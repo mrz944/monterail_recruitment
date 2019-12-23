0394f0338cb93ce4af290be67c643c55fea83f79
Marcin Zadka <gamboox@gmail.com>
2019-12-23 13:57:35 +0100

    Fix ReservationPlacesValidator

--- app/validators/reservation_places_validator.rb
+++ app/validators/reservation_places_validator.rb
@@ -6,7 +6,7 @@ class ReservationPlacesValidator < ActiveModel::Validator
       record.reservation.errors[:base] << "Places value must be even!"
     elsif record.ticket_type.all_together? && record.places != record.ticket_type.places
       record.reservation.errors[:base] << "Places value must be equal to places of this ticket type!"
-    elsif record.ticket_type.avoid_one? && record.ticket_type.available_places - record.places > 1
+    elsif record.ticket_type.avoid_one? && record.ticket_type.available_places - record.places == 1
       record.reservation.errors[:base] << "Can't leave only 1 available place for this ticket type!"
     end
   end
